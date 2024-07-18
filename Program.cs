01029a1c4429b7e5e955fd23e15bf88596409398
Aslambek_Dombaev <aslambekdombaev@mail.ru>
2024-07-18 16:28:55 +0300

    Заполнение элементов массива

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string[] ArrayElements(int b)
+{
+    string[] arr = new string[b];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine($"{i + 1} элемент массива: ");
+        arr[i] = Console.ReadLine()!;
+    }
+    return arr;
+}
+
+
+
+Console.WriteLine("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string[] arr = ArrayElements(a);
+
+Console.WriteLine(arr);
